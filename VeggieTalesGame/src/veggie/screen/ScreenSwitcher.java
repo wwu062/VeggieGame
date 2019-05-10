24c6c51613eb8ee52ab2c33cc3991ee4fd6092d8
wwu062 <wwu062@PR19.hhs.win.fuhsd.org>
2019-05-10 10:30:15 -0700

    added javadocs

--- VeggieTalesGame/src/veggie/screen/ScreenSwitcher.java
+++ VeggieTalesGame/src/veggie/screen/ScreenSwitcher.java
@@ -2,11 +2,26 @@ package veggie.screen;
 
 
 public interface ScreenSwitcher {
+	/**
+	 * the integer code of the menu screen
+	 */
+	
 	public static final int MENU = 0;
+	/**
+	 * the integer code of the instruction screen
+	 */
+	
 	public static final int INSTRUCTION = 1;
+	
+	/**
+	 * the integer code of the platformMode screen
+	 */
 	public static final int PLATFORM = 2;
+	
+	/**
+	 * the integer code of the battleMode screen
+	 */
 	public static final int BATTLE = 3;
-
 	
 	public void switchScreen(int i);
 }
