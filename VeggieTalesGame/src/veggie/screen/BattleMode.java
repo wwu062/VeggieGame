a7089a52d24b8261eb8b2328c865740a4a64740e
williamwu <williamwu@10.16.93.147>
2019-05-14 16:48:59 -0700

    added new gif

--- VeggieTalesGame/src/veggie/screen/BattleMode.java
+++ VeggieTalesGame/src/veggie/screen/BattleMode.java
@@ -1,5 +1,6 @@
 package veggie.screen;
 
+import java.awt.Point;
 import java.awt.Rectangle;
 
 import veggie.model.Entity;
@@ -69,35 +70,53 @@ public class BattleMode extends Screen {
 		surface.pushStyle();
 		surface.rect(button[1].x, button[1].y, button[1].width, button[1].height);
 		surface.fill(0);
-		String a = player.getMoveList()[1].getName();
-		float w = surface.textWidth(a);
-		surface.text(a, button[1].x + button[1].width / 2 - w / 2, button[1].y + button[1].height / 2);
+		String move1 = player.getMoveList()[1].getName();
+		float w = surface.textWidth(move1);
+		surface.text(move1, button[1].x + button[1].width / 2 - w / 2, button[1].y + button[1].height / 2);
 		surface.popStyle();
 
 		surface.pushStyle();
 		surface.rect(button[2].x, button[2].y, button[2].width, button[2].height);
 		surface.fill(0);
-		String b = player.getMoveList()[2].getName();
-		float y = surface.textWidth(b);
-		surface.text(b, button[2].x + button[2].width / 2 - y / 2, button[2].y + button[2].height / 2);
+		String move2 = player.getMoveList()[2].getName();
+		float y = surface.textWidth(move2);
+		surface.text(move2, button[2].x + button[2].width / 2 - y / 2, button[2].y + button[2].height / 2);
 		surface.popStyle();
 
 		surface.pushStyle();
 		surface.rect(button[3].x, button[3].y, button[3].width, button[3].height);
 		surface.fill(0);
-		String c = player.getMoveList()[3].getName();
-		float d = surface.textWidth(c);
-		surface.text(c, button[3].x + button[3].width / 2 - d / 2, button[3].y + button[3].height / 2);
+		String move3 = player.getMoveList()[3].getName();
+		float d = surface.textWidth(move3);
+		surface.text(move3, button[3].x + button[3].width / 2 - d / 2, button[3].y + button[3].height / 2);
 		surface.popStyle();
 
 		surface.pushStyle();
 		surface.rect(button[4].x, button[4].y, button[4].width, button[4].height);
 		surface.fill(0);
-		String e = player.getMoveList()[4].getName();
-		float f = surface.textWidth(e);
-		surface.text(b, button[4].x + button[4].width / 2 - f / 2, button[4].y + button[4].height / 2);
+		String move4 = player.getMoveList()[4].getName();
+		float f = surface.textWidth(move4);
+		surface.text(move4, button[4].x + button[4].width / 2 - f / 2, button[4].y + button[4].height / 2);
 		surface.popStyle();
+		
+		
+
+	}
+	
+	/**
+	 * checks if mouse is being pressed and switches screens if it is on a button
+	 */
+	public void mousePressed() {
+		Point p = surface.actualCoordinates(new Point(surface.mouseX, surface.mouseY));
+		if (button[1].contains(p))
+			surface.switchScreen(ScreenSwitcher.PLATFORM);
+		if (button[2].contains(p))
+			surface.switchScreen(ScreenSwitcher.INSTRUCTION);
+		if (button[3].contains(p))
+			
+		if (button[4].contains(p))
 
 	}
 
+	
 }
